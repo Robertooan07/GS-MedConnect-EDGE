81e5a1d403c233172d1ac213eeea4930d4ee2c31
Robertooan07 <RM551460@fiap.com.br>
2023-11-22 01:24:01 -0300

    Codigo do projéto .ino e add README

---
+++ Index.ino
@@ -0,0 +1,129 @@
+#include <ArduinoJson.h>
+#include <LiquidCrystal.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const char* mqttServer = "mqtt.tago.io";
+const int mqttPort = 1883;
+const char* mqttUser = "Default";
+const char* mqttPassword = "3ffcf440-c105-4b8b-ae95-6074c7550a57";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Parte do código LCD
+LiquidCrystal lcd(5, 17, 4, 0, 2, 15);
+
+int redLed = 16;
+int trava = 13;
+int successButton = 18;
+int failedButton = 19;
+
+void setup()
+{
+  Serial.begin(9600);
+  connectToWiFi();
+  connectToMQTT();
+  
+  lcd.begin(20, 4); // Inicializa o LCD com 20 colunas por 4 linhas
+  pinMode(successButton, INPUT);
+  pinMode(failedButton, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(trava, OUTPUT);
+  initialMessage();
+}
+
+void loop()
+{
+  client.loop();
+
+  if (digitalRead(successButton) == HIGH)
+  {
+    lcd.clear();
+    lcd.print("Nome: Roberto");
+    lcd.setCursor(0, 1);
+    lcd.print("Oliveira");
+    lcd.setCursor(0, 2);
+    lcd.print("CPF:12627603801");
+    lcd.setCursor(0, 3);
+    lcd.print("clique no amarelo ->");
+    digitalWrite(trava, HIGH);
+    delay(5000);
+    enviarDadosParaTago("Roberto Oliveira", "12627603801");
+    initialMessage();
+    digitalWrite(trava, LOW);
+  }
+  else if (digitalRead(failedButton) == HIGH)
+  {
+    lcd.clear();
+    digitalWrite(redLed, HIGH);
+    lcd.print("Telefone: 999312123");
+    lcd.setCursor(0, 1);
+    lcd.print("Plano: NotreDame");
+    lcd.setCursor(0, 2);
+    lcd.print("Intermedica");
+    lcd.setCursor(0, 3);
+    lcd.print("Boa Consulta :)");
+    delay(5000);
+    enviarDadosParaTago("999312123", "NotreDame Intermedica");
+    initialMessage();
+    digitalWrite(redLed, LOW);
+  }
+}
+
+void initialMessage()
+{
+  lcd.clear();
+  lcd.print("Aguardando RFID");
+  lcd.setCursor(0, 1);
+}
+
+void enviarDadosParaTago(String dado1, String dado2)
+{
+  DynamicJsonDocument jsonDoc(1024);
+  JsonObject root = jsonDoc.to<JsonObject>();
+
+  root["dado1"] = dado1;
+  root["dado2"] = dado2;
+
+  String payload;
+  serializeJson(root, payload);
+
+  client.publish("tago/data", payload.c_str());
+  Serial.println("Dados enviados para Tago.io: " + payload);
+}
+
+void connectToWiFi()
+{
+  Serial.println("Conectando WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Conectado");
+}
+
+void connectToMQTT()
+{
+  Serial.println("Conectando com Servidor MQTT");
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected())
+  {
+    if (client.connect("esp32", mqttUser, mqttPassword))
+    {
+      Serial.println("MQTT Conectado");
+    }
+    else
+    {
+      Serial.print("Falha na conexão com MQTT, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5 segundos...");
+      delay(5000);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GS-MedConnect-EDGE
+
+# Problema Abordado
+O projeto aborda a problematica no contexto da recepção em ambientes médicos, onde o processo de coleta de informações dos pacientes muitas vezes é moroso e propenso a erros. A necessidade de otimizar a eficiência e a precisão nesse fluxo torna-se evidente, uma vez que a agilidade na entrada de dados é crucial para garantir um atendimento médico eficaz. O desafio reside em proporcionar uma solução inovadora que simplifique a coleta de informações, reduzindo o tempo de espera do paciente e melhorando a qualidade do serviço prestado pela recepção.
+
+# Visão Geral da Solução Proposta
+A solução proposta neste projeto visa transformar positivamente o processo de recepção em ambientes médicos por meio da implementação de um sistema baseado em cartões RFID simulados. Ao utilizar a tecnologia de identificação por radiofrequência, o paciente pode simplesmente passar seu cartão próximo a um leitor RFID para que suas informações, como nome, CPF e outros detalhes relevantes, sejam instantaneamente exibidas no sistema de recepção. Essa abordagem não apenas agiliza a coleta de dados, reduzindo potencialmente filas e atrasos, mas também minimiza a probabilidade de erros de digitação, proporcionando uma experiência mais eficiente e precisa tanto para a equipe de recepção quanto para os pacientes.
+
+# Instruções Claras Sobre Como Configurar e Executar a Aplicação
